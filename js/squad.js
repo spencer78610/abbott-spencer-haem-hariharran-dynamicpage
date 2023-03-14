1ac68a1a9345c81ee459d77ba3c5cb23b2740068
spencer78610 <s_abbott150464@fanshaweonline.ca>
2023-03-14 15:02:54 -0400

    added basic java
    debugger works

---
+++ js/squad.js
@@ -0,0 +1,14 @@
+
+let squad  = {
+    spencer: {
+        name: 'Spencer Abbott',
+        title: 'C.E.O',
+        bio: 'some interesting bio stuff'
+    },
+
+    haem: {
+        name: 'Haem Hariharran',
+        title: 'Vice President',
+        bio: 'some interesting bio stuff'
+    }
+}
