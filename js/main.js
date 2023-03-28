6dfb920fdc8fd6f8e2488632b2df6c103da732ac
spencer78610 <s_abbott150464@fanshaweonline.ca>
2023-03-28 01:27:23 -0400

    added some css

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 console.log('js is working');
 
 let inputButton = document.querySelectorAll('#team button'),
-    thebox = document.querySelector('#info-team');
+    thebox = document.querySelector('#info-team'),
+    currentBoxButton = null;
+
 
     function getboxData() {
-        thebox.style.display = "block",
-        thebox.querySelector('.ib-title').textContent = squad[this.dataset.member].title,
-        thebox.querySelector('.ib-heading').textContent = squad[this.dataset.member].name,
-        thebox.querySelector('.ib-text').textContent = squad[this.dataset.member].bio;
-
-        console.log('button is working');
+        if (currentBoxButton === this) { 
+            thebox.style.display = "none";
+            currentBoxButton = null;
+          } else {
+            thebox.style.display = "block";
+            thebox.querySelector('.ib-title').textContent = squad[this.dataset.member].title;
+            thebox.querySelector('.ib-heading').textContent = squad[this.dataset.member].name;
+            thebox.querySelector('.ib-text').textContent = squad[this.dataset.member].bio;
+            currentBoxButton = this;
+          }
     }
 
     inputButton.forEach(button => button.addEventListener('click', getboxData));
--- js/squad.js
+++ js/squad.js
@@ -2,13 +2,13 @@
 let squad  = {
     spencer: {
         name: 'Spencer Abbott',
-        title: 'C.E.O',
-        bio: 'some interesting bio stuff'
+        title: 'Coder',
+        bio: 'Owner of the new 2023 re-branded Dr.Nut. Currently attending Fanshawe College for Interact Media Desgin. Looking to sharpen his skills and expand his resume with a dipolma'
     },
 
     haem: {
         name: 'Haem Hariharran',
-        title: 'Vice President',
+        title: 'UX & UI Desginer',
         bio: 'some interesting bio stuff',
         
     }
